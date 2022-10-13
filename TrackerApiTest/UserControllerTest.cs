15fe62f63f3e88cc3e77d0c672dfec073fb155d7
Guilherme Santos (VIA Consulting) <guilherme.santos@viaconsultingway.com>
2022-10-13 11:38:58 +0100

    update teste

--- TrackerApiTest/UserControllerTest.cs
+++ TrackerApiTest/UserControllerTest.cs
@@ -1,7 +1,11 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerApi.Controllers;
@@ -14,6 +18,49 @@ namespace TrackerApiTest
 {
     public class UserControllerTest
     {
+
+        [Fact]
+        public async Task GetUsers_ShouldReturn_OK()
+        {
+            Moq.Mock<IUserService> mock = new Moq.Mock<IUserService>();
+            mock.Setup(x => x.GetAll(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult(It.IsAny<GetUsersViewModel>()));
+
+            var userController = new UserController(mock.Object);
+
+
+            var result = ((IStatusCodeActionResult)userController.GetAsync(0, 25).Result).StatusCode.Value;
+
+            result.Should().Be((int)HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public void GetUsers_ShouldReturn_BadRequest()
+        {
+            Moq.Mock<IUserService> mock = new Moq.Mock<IUserService>();
+            mock.Setup(x => x.GetAll(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult(It.IsAny<GetUsersViewModel>()));
+
+            var userController = new UserController(mock.Object);
+
+
+            var result = ((IStatusCodeActionResult)userController.GetAsync(0, 10000).Result).StatusCode.Value;
+
+            result.Should().Be((int)HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public void GetUsers_ShouldReturn_Unathorized()
+        {
+            Moq.Mock<IUserService> mock = new Moq.Mock<IUserService>();
+            mock.Setup(x => x.GetAll(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult(It.IsAny<GetUsersViewModel>()));
+
+            var userController = new UserController(mock.Object);
+
+
+            var result = ((IStatusCodeActionResult)userController.GetAsync(0, 10000).Result).StatusCode.Value;
+
+            result.Should().Be((int)HttpStatusCode.BadRequest);
+        }
+
         [Fact]
         public void MustCreateUser()
         {
@@ -34,30 +81,6 @@ namespace TrackerApiTest
             var result = (Microsoft.AspNetCore.Mvc.CreatedResult)userController.PostAsync(testObject).Result;
 
             Assert.True(result.StatusCode == 201);
-        }
-
-        [Fact]
-        public void Should_Return_UserAlreadyExists()
-        {
-            var cardMock = new Mock<User>();
-
-            var testObject = new CreateUserViewModel()
-            {
-                Name = "sdsd",
-                Email = "sdsdsd",
-                Password = "sdsdsd"
-            };
-
-            Moq.Mock<IUserService> mock = new Moq.Mock<IUserService>();
-            mock.Setup(x => x.Create(testObject)).Returns(Task.FromResult(cardMock.Object));
-
-            var userController = new UserController(mock.Object);
-
-            var result = (Microsoft.AspNetCore.Mvc.CreatedResult)userController.PostAsync(testObject).Result;
-
-            var result2 = (Microsoft.AspNetCore.Mvc.CreatedResult)userController.PostAsync(testObject).Result;
-
-            Assert.True(result2.StatusCode != 201);
         }
     }
 }
